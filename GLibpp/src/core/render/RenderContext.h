164deab559dad64c27e73112be679af9549c776e
Matej Uzel <uzel@happyend.cz>
2026-03-05 16:24:29 +0100

    render context

---
+++ GLibpp/src/core/render/RenderContext.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "RenderCommand.h"
+
+class RenderContext {
+public:
+
+	RenderContext(WindowBuilder* window) : 
+		window(window),
+		viewport{ 0, 0, 800, 600 },
+		projection{ Mtx4::identity() },
+		modelview{ Mtx4::identity() }
+	{}
+
+	void registerMesh(Mesh* mesh, uint32_t meshId) {
+		meshes[meshId] = mesh;
+	}
+
+	Mesh* getMesh(uint32_t meshId) const {
+		
+		auto it = meshes.find(meshId);
+		if (it == meshes.end()) return nullptr;
+		return it->second;
+	}
+
+	void drawMesh(uint32_t meshId) {
+		
+		Mesh* mesh = getMesh(meshId);
+
+		if (mesh != nullptr) 
+		{
+			Mtx4 mvp = projection * modelview * mesh->transformation;
+			//window.drawMesh(window, viewport, mvp, mesh); // @todo
+		}
+	}
+
+private:
+
+	WindowBuilder* window;
+	std::unordered_map<uint32_t, Mesh*> meshes;
+	Viewport viewport;
+	Mtx4 projection;
+	Mtx4 modelview;
+};
