c6ad8406593afab112aa66b358f2b28c413afa51
Matej Uzel <uzel@happyend.cz>
2026-05-27 18:50:18 +0200

    vyvoj - vytvareni primitiv

--- GLibpp/src/Engine/Geometry/MeshFactory.h
+++ GLibpp/src/Engine/Geometry/MeshFactory.h
@@ -5,12 +5,12 @@
 class MeshFactory {
 
 public:
-	static Mesh CreateCube(float side);
-	static Mesh CreateSphere(float radius, uint32_t segments = 16);
-	static Mesh CreateCylinder(float radius, float height, uint32_t segments);
-	static Mesh CreateIcosan(float radius);
-	static Mesh CreateIcosphere(float radius, uint32_t subdivisions);
-	static Mesh CreateNet(uint32_t size, float distort = 0.0f);
-	static Mesh CreateNetWave(uint32_t size, float waveHeight = 0.5f, float time = 0.0f, float speed = 1.0f);
-	static Mesh CreateQuad(float size);
+	static Mesh CreateCube(float side = 1.0f);
+	static Mesh CreateSphere(float radius = 1.0f, uint32_t segments = 16);
+	static Mesh CreateCylinder(float radius = 1.0f, float height = 1.0f, uint32_t segments = 16);
+	static Mesh CreateIcosan(float radius = 1.0f);
+	static Mesh CreateIcosphere(float radius = 1.0f, uint32_t subdivisions = 1);
+	static Mesh CreateNet(uint32_t size = 1, float distort = 0.0f);
+	static Mesh CreateNetWave(uint32_t size = 1, float waveHeight = 0.5f, float time = 0.0f, float speed = 1.0f);
+	static Mesh CreateQuad(float size = 1.0f);
 };
